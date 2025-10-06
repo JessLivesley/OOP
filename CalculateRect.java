2e40f60167f8cbd07d382226e1cbb761e3faf9cc
JessLivesley <jesslivesley@icloud.com>
2025-10-06 15:18:26 +0000

    Inital commit

---
+++ CalculateRect.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CalculateRect{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter rectangle length: ");
+        double length = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Enter rectangle width: " );
+        double width = Double.parseDouble(scanner.nextLine());
+
+        double area = length * width;
+        double perimeter = 2 * (length + width);
+
+        System.out.println("The Area of the rectangle is:  " + area);
+        System.out.println("The Perimeter of the rectangle is: " + perimeter);
+
+        scanner.close();
+    }       
+}
